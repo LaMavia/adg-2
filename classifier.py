ed47232a540b434e632a1cc49ade6b0abbb849a4
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-11 18:38:31 +0100

    add read

--- classifier.py
+++ classifier.py
@@ -1,18 +1,24 @@
 import sys
-import csv
 import pandas as pd
-from typing import Iterator
+from Bio import SeqIO 
+import gzip
 
 def training_sets(path: str):
     datasets = pd.read_csv(path, delimiter="\t")
-    for data_path, data_class in datasets:
-        print(f"{data_path=}, {data_class=}")
+    for _, (dpath, dcls, *_) in datasets.iterrows():
+        print(f'{dpath=}, {dcls=}')
+        open_fun = gzip.open if dpath.endswith('.gz') else open
 
+        with open_fun(dpath, 'rt') as handle:
+            yield dcls, (str(record['seq']) for record in SeqIO.parse(handle, "fasta"))
 
 def main():
     # python3 classifier.py training_data.tsv testing_data.tsv output.tsv
     _, training_path, testinng_path, output_path = sys.argv
 
     training_sets(training_path)
+
+if __name__ == "__main__":
+    main()
     
 
